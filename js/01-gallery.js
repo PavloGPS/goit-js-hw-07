ea4939452b2c9c5bea420a40af10926e7a07c1f4
Pavlo Grigoriev <gpavlo2020@gmail.com>
2023-04-05 22:01:28 +0300

    Update 01-gallery.js
    refactor
    uses onShow and onClose

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -30,18 +30,24 @@ function onGalleryElClick(evt) {
   if (evt.target.nodeName !== "IMG") {
     return;
   }
-  const instance = basicLightbox.create(`
+  const instance = basicLightbox.create(
+    `
     <img src="${evt.target.dataset["source"]}" width="800" height="600">
-  `);
+  `,
+    {
+      onShow: (instance) => {
+        galleryEl.addEventListener("keydown", onEscKeydown);
+      },
+      onClose: (instance) => {
+        galleryEl.removeEventListener("keydown", onEscKeydown);
+        console.log("cosed and remooved event listener from Esc");
+      },
+    }
+  );
   instance.show();
-
-  galleryEl.addEventListener("keydown", onEscKeydown);
-
   function onEscKeydown(evt) {
     if (evt.code === "Escape") {
-      instance.close(() => {
-        galleryEl.removeEventListener("keydown", onEscKeydown);
-      });
+      instance.close();
     }
   }
 }
